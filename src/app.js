be103db0b7be0ded53e18d85a03603b2dc21a248
Ashok Mishra <ashokmishra1709@gmail.com>
2025-12-18 12:53:33 +0530

    Fix CORS for Vercel Deployment

--- src/app.js
+++ src/app.js
@@ -14,7 +14,10 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all for Vercel deployment debugging
+  credentials: true
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
